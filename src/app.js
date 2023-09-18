172884c890bba6155b28b9249a0fd9066af9aa45
sarker25 <shahadathbinoy25@outlook.com>
2023-09-18 12:54:35 +0600

    global middleware add

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const applyMiddleware = require("./middlewares");
 
 const app = express();
+
+// middleware
+applyMiddleware(app);
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
---
+++ src/middlewares/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const applyMiddleware = (app) => {
+  app.use(express.json());
+};
+
+module.exports = applyMiddleware;
